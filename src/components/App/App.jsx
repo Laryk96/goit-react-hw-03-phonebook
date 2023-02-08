729d8e19d2c18fd3467ca603f304a778a78b7ddc
Lary <lary96bk@gmail.com>
2023-02-09 00:10:46 +0200

    set localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,33 @@
-import React, { Component } from "react";
-import { Report } from "notiflix";
+import React, { Component } from 'react';
+import { Report } from 'notiflix';
 
-import Filter from "../Filter";
-import ContactForm from "components/ContactForm";
-import ContactList from "components/ContactList";
-import { Title, Container, SubTitle } from "./App.styled";
+import Filter from '../Filter';
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import { Title, Container, SubTitle } from './App.styled';
+
+const KEY_CONTACTS = 'name/tell';
 
 class App extends Component {
   state = {
     contacts: [],
-    filter: "",
+    filter: '',
   };
 
-  getContactForm = (contact) => {
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem(KEY_CONTACTS));
+
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(KEY_CONTACTS, JSON.stringify(this.state.contacts));
+    }
+  }
+
+  getContactForm = contact => {
     const restulChack = this.chackName(contact.name);
 
     if (restulChack) {
@@ -22,13 +37,13 @@ class App extends Component {
     }
   };
 
-  handleFilter = (e) => {
+  handleFilter = e => {
     const filter = e.currentTarget.value;
 
     this.setState({ filter: filter });
   };
 
-  chackName = (newName) => {
+  chackName = newName => {
     if (this.state.contacts.some(({ name }) => name === newName)) {
       Report.warning(`${newName} is already in contacts`);
       return false;
@@ -37,7 +52,7 @@ class App extends Component {
     return true;
   };
 
-  deleteContact = (contactId) => {
+  deleteContact = contactId => {
     this.setState(({ contacts }) => ({
       contacts: contacts.filter(({ id }) => id !== contactId),
     }));
